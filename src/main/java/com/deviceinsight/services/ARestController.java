4f7e5391b6c0623017ca996a24c95f31e1b68428
Simon Ebner <simon.ebner@next-cloud.de>
2017-02-16 21:03:35 +0100

    first time that hazelcast jet got running

--- src/main/java/com/deviceinsight/services/ARestController.java
+++ src/main/java/com/deviceinsight/services/ARestController.java
@@ -1,5 +1,7 @@
 package com.deviceinsight.services;
 
+import com.hazelcast.jet.Jet;
+import com.hazelcast.jet.JetInstance;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,12 @@ public class ARestController {
     @RequestMapping("/test")
     public String test() throws ExecutionException, InterruptedException {
 
+
+        JetInstance instance1 = Jet.newJetClient();
+
+
+
+        System.out.println(instance1.getConfig().toString());
 
         return "test";
 
