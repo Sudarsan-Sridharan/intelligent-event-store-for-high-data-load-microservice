f12fa0900fdbb231276b1ff6d5a16636fb4261db
simon-ebner <mail@simon-ebner.de>
2017-06-04 17:59:58 +0200

    assertJ with sample added

--- src/test/java/com/deviceinsight/services/CreateClientIntegrationTest.java
+++ src/test/java/com/deviceinsight/services/CreateClientIntegrationTest.java
@@ -1,30 +1,19 @@
 package com.deviceinsight.services;
 
 
-import com.deviceinsight.services.model.Client;
 import com.deviceinsight.services.model.Restaurant;
 import com.deviceinsight.services.model.dao.RestaurantDao;
-import com.deviceinsight.services.model.dao.RestaurantDaoImpl;
-import com.google.common.collect.LinkedListMultimap;
-import com.google.common.collect.Lists;
-import org.junit.*;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.ObjectToStringHttpMessageConverter;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(MockitoJUnitRunner.class) // SpringRunner
@@ -64,6 +53,8 @@ public class CreateClientIntegrationTest {
 restaurantDao.list();
 
         assertEquals(((Restaurant) restaurantDao.list().get(0)).getTitle(), ((Restaurant) mockedList.get(0)).getTitle());
+
+assertThat("abaa").contains("b");
 
 
         /*ResponseEntity<String> responseEntity =
